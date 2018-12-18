dd3b6c2828573b90ccd5d11e19fbc898d57fdac2
markcor <mcornmesser@mozilla.com>
2018-12-17 16:01:02 -0800

    k

--- manifests/disable_windows_defender.pp
+++ manifests/disable_windows_defender.pp
@@ -8,16 +8,16 @@ class win_defend::disable_windows_defender {
 	registry_value { 'hklm\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching\SearchOrderConfig' :
 		ensure  => present,
 		type    => dword,
-		value   => "0",
+		data    => "0",
 	}
 	registry_value { 'hklm\SOFTWARE\Microsoft\Windows\Windows\AU\NoAutoUpdate'  :
 		ensure => present,
 		type   => dword,
-		value  => "1",
+		data   => "1",
 	}
 	registry_value { 'hklm\SOFTWARE\Microsoft\Windows\Windows\AU\AUOptions' :
 		ensure => present,
 		type   => dword,
-		value  => "2",
+		data   => "2",
 	}
 }
